c520b282f541e9ef8967ab69311fa36b5b936a04
sarahmmedd <sara.ahmed.iteach@gmail.com>
2026-02-25 00:26:30 +0200

    Updating the UI

--- lib/features/expenses/ui/expenses_screen.dart
+++ lib/features/expenses/ui/expenses_screen.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:banawit/core/theme/app_colors.dart';
 
 class ExpensesScreen extends StatelessWidget {
   const ExpensesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.scaffoldBackground,
+      body: Column(
+        children: [
+          /// HEADER
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.only(
+              top: 25.h,
+              left: 20.w,
+              right: 20.w,
+              bottom: 30.h,
+            ),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFFFF5C8A), Color(0xFFFF2A5F)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+            child: Row(
+              children: [
+                /// Back Arrow
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.2),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 20.w),
+
+                /// Title
+                Text(
+                  "Add Expense",
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
